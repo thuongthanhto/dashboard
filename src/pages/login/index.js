64ed5d20f23e7f5b0aa3241f1ffb07ba81e84a43
Thuong To <thuongthanhto@gmail.com>
2022-09-15 00:07:00 +0700

    Update

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,7 +7,7 @@ const Login = () => {
             <img
               src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
               className="w-full"
-              alt="Sample image"
+              alt="cover"
             />
           </div>
           <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
